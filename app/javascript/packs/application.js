558644c23cf8758971aedae5851c7a9217fa91ce
etoribadanai <etoribadanai@gmail.com>
2020-08-10 20:02:27 -0300

    show value in screen with js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 
 import "../stylesheets/application"
+require('./exchanges')
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
